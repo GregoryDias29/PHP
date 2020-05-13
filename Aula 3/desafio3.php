339d4b164a3cd48d0ba433566d6e78830242c96d
Gregory Dias <gregorydiassony@gmail.com>
2020-05-13 14:27:03 -0300

    adicionando desafio

---
+++ Aula 3/desafio3.php
@@ -0,0 +1,33 @@
+<?php
+    $arr = [
+                "1"=>"Namibia,", 
+                "2"=>"Nauru,", 
+                "3"=>"Nepal",
+                "4"=>"Netherlands",
+                "5"=>"New Zealand",
+                "6"=>"Nicaragua",
+                "7"=>"Niger",
+                "8"=>"Nigeria",
+                "9"=>"North Korea",
+                "10"=>"Norway"
+    ];
+    
+    echo $arr[1];
+    echo ",";
+    echo$arr[2];
+    echo ",";
+    echo $arr[3];
+    echo ",";
+    echo $arr[4];
+    echo ",";
+    echo $arr[5];
+    echo ",";
+    echo $arr[6];
+    echo ",";
+    echo $arr[7];
+    echo ",";
+    echo $arr[8];
+    echo ",";
+    echo $arr[9];
+    echo ",";
+    echo $arr[10];
