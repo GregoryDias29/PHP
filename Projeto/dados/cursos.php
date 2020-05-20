ed1bdaf4f363d03b6a132e0e3778c36a5077821d
Gregory Dias <gregorydiassony@gmail.com>
2020-05-20 11:57:54 -0300

    Adicionando projeto final

---
+++ Projeto/dados/cursos.php
@@ -0,0 +1,33 @@
+<?php
+
+function cursos(){
+    return[
+        "php" => [
+            "nome_curso" => "Curso de PHP",
+            "versao_ferramenta" => 7.4,
+            "carga_horaria" => 40,
+            "status" => true 
+        ],
+    
+        "CSS" => [
+            "nome_curso" => "Curso de CSS",
+            "versao_ferramenta" => 5,
+            "carga_horaria" => 15,
+            "status" => true 
+        ],
+
+        "JS" => [
+            "nome_curso" => "Curso de JS",
+            "versao_ferramenta" => 11.5,
+            "carga_horaria" => 30,
+            "status" => true 
+        ],
+        "Jquery" => [
+            "nome_curso" => "Curso de Jquary",
+            "versao_ferramenta" => 15.5,
+            "carga_horaria" => 35,
+            "status" => false 
+        ],
+
+    ];
+}
---
+++ Projeto/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once "uteis/formatador.php"; // tudo tem que ser colocado antes do principal se não ele não acha 
+require_once "dados/cursos.php";
+require_once "exibicao/principal.php";
